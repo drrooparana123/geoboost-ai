3b65d82cf1c71dfb39581ce35de1d04c930a0a3e
drrooparana123 <drrooparana@gmail.com>
2026-01-29 23:14:19 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  publicRoutes: ["/"]
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!_next|.*\\..*).*)"]
 };
