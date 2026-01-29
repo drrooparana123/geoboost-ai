64c1c0388eecf3f62f82508e34ca28f90165b290
drrooparana123 <drrooparana@gmail.com>
2026-01-29 23:12:33 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,12 @@
-import { UserButton, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Dashboard() {
   const user = await currentUser();
 
   return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+    <main style={{ padding: 40 }}>
       <h1>Dashboard</h1>
       <p>Welcome {user?.firstName}</p>
-      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
