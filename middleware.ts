2fc841d03b8af8afcca1e5e61ad4af13650c9e50
drrooparana123 <drrooparana@gmail.com>
2026-01-30 08:39:22 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)"],
+  matcher: [
+    /*
+     * Match all routes except:
+     * - static files
+     * - Next.js internals
+     */
+    "/((?!_next|.*\\..*).*)",
+  ],
 };
